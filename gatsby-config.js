1cd9054222d8ef4e5f88328e23a5d2efe930521b
Carlos Eduardo Valdez Quintín <carlos@carlosvq.xyz>
2019-06-11 21:33:58 -0400

    update config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,28 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Bitpharma`,
-    description: `Bitpharma official website.`,
-    author: `@carlosvq`,
+    title: "Bitpharma Site",
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    "gatsby-plugin-react-helmet",
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
       },
     },
     {
@@ -31,6 +17,19 @@ module.exports = {
         rule: {
           include: /\.inline\.svg$/,
         },
+      },
+    },
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "bitpharma-site",
+        short_name: "starter",
+        start_url: "/",
+        background_color: "#663399",
+        theme_color: "#663399",
+        display: "minimal-ui",
       },
     },
     "gatsby-plugin-styled-components",
